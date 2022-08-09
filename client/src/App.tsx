63aee3058107833cf94268fcabf61a735f53bb32
minsun <judy.leems@gmail.com>
2022-08-09 23:44:51 +0900

    Feat: client setting

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
+import GlobalStyle from "./styles/globalStyle";
+import GlobalFonts from "./styles/fonts/fonts";
+import Router from "./router";
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalFonts />
+      <GlobalStyle />
+      <Router />
+    </ThemeProvider>
+  );
+}
+
+export default App;
